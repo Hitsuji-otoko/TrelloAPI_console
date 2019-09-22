cc67026d530ad107f780ddaad0aecffd6b2f0436
Hitsuji-otoko <shmahoro@gmail.com>
2019-09-22 20:56:58 +0900

    only-trelloAPI

---
+++ main.rb
@@ -0,0 +1,35 @@
+require 'trello'
+require 'pry'
+require 'dotenv/load'
+
+
+def puts_todos
+    configure
+    cards = get_cards
+
+    puts '---------------------------'
+    puts '【TODO】' 
+    puts '|カード名 :' + "#{@list_name}" + '|'
+    cards.each do |card|
+        puts card.name
+    end
+    puts '---------------------------'
+end
+
+def configure
+    Trello.configure do |config|
+        config.developer_public_key = ENV['DEVELOPER_PUBLIC_KEY']
+        config.member_token = ENV['MEMBER_TOKEN']
+    end
+end
+
+def get_cards
+    member = Trello::Member.find('happy_ryosuke16')
+    boards = member.boards
+    board = boards.find { |board| board.name == 'TODO'}
+    list = board.lists[0]
+    @list_name = list.name
+    cards = list.cards
+end
+
+puts_todos
